2500ea04088f4047ede1c627e9dcb72d8d726637
Tobias Ortmayr <tortmayr@eclipsesource.com>
2023-01-03 16:00:33 +0100

    Fix issues with copy&paste feature (#35)
    * Update glsp-config
    
    Update to latest glsp-config version & autformat package.json files 
    Add check:pr & check:header scripts
    
    * Fix issues with copy&paste feature
    
    - Fix incorrect edge rewiring in `CopyPasteOperationHandler`
    - Fix incorrect usage of assignment (=) instead of comparison (===) in   typeguard functions of graph package
    - Update feature matrix in README.md
    
    Fixes https://github.com/eclipse-glsp/glsp/issues/841

--- packages/graph/src/glayouting.ts
+++ packages/graph/src/glayouting.ts
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2022 STMicroelectronics and others.
+ * Copyright (c) 2022-2023 STMicroelectronics and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -23,7 +23,7 @@ export interface GLayouting {
 }
 
 export function isGLayouting<G extends GModelElement>(element: G): element is G & GLayouting {
-    return ((element as any)[GLayouting] = true);
+    return GLayouting in element && element[GLayouting] === true;
 }
 
 export type GLayoutingBuilder = GModelElementBuilder<GModelElement & GLayouting>;
