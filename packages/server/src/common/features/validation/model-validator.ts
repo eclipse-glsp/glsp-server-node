5015e903466030583e408a0d4e9c8ba003d38198
Philip Langer <planger@eclipsesource.com>
2023-04-24 16:14:07 +0200

    Distinguish between live and batch validation
    https://github.com/eclipse-glsp/glsp/issues/980
    
    Apply suggestions from code review
    
    Co-authored-by: Tobias Ortmayr <tortmayr@eclipsesource.com>

--- packages/server/src/common/features/validation/model-validator.ts
+++ packages/server/src/common/features/validation/model-validator.ts
@@ -14,7 +14,8 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 import { GModelElement } from '@eclipse-glsp/graph';
-import { Marker, MaybePromise } from '@eclipse-glsp/protocol';
+import { Marker, MarkersReason, MaybePromise } from '@eclipse-glsp/protocol';
+import { injectable } from 'inversify';
 
 export const ModelValidator = Symbol('ModelValidator');
 
@@ -28,7 +29,78 @@ export interface ModelValidator {
      * Validates the given list of {@link GModelElement}s and returns a list of {@link Marker}s.
      *
      * @param elements The list of {@link GModelElement} to validate.
-     * @returns A list of {@link Marker}s for the validated {@link GModelElement}s.
+     * @param reason The reason for a validation request, such as "batch" or "live" validation.
+     * @return A list of {@link Marker}s for the validated {@link GModelElement}s.
      */
-    validate(elements: GModelElement[]): MaybePromise<Marker[]>;
+    validate(elements: GModelElement[], reason?: string): MaybePromise<Marker[]>;
+}
+
+@injectable()
+export abstract class AbstractModelValidator implements ModelValidator {
+    validate(elements: GModelElement[], reason: string): Marker[] {
+        const markers: Marker[] = [];
+        for (const element of elements) {
+            if (MarkersReason.LIVE === reason) {
+                markers.push(...this.doLiveValidation(element));
+            } else if (MarkersReason.BATCH === reason) {
+                markers.push(...this.doBatchValidation(element));
+            } else {
+                markers.push(...this.doValidationForCustomReason(element));
+            }
+            if (element.children) {
+                markers.push(...this.validate(element.children, reason));
+            }
+        }
+        return markers;
+    }
+
+    /**
+     * Perform the live validation rules for the given <code>element</code>.
+     *
+     * This will be invoked on start and after each operation for all elements.
+     * Thus, the validation should be rather inexpensive.
+     * There is no need to traverse through the children in this method as {@link #validate(List, String)} will invoke
+     * this method for all children anyway.
+     *
+     * @param element The element to validate.
+     * @return A list of {@link Marker}s for the validated {@link GModelElement}.
+     */
+    doLiveValidation(element: GModelElement): Marker[] {
+        return [];
+    }
+
+    /**
+     * Perform the batch validation rules for the given <code>element</code>.
+     *
+     * <p>
+     * This will be invoked on demand by the client.
+     * Thus, the validation can include more expensive validation rules.
+     * There is no need to traverse through the children in this method as {@link #validate(List, String)} will invoke
+     * this method for all children anyway.
+     * </p>
+     *
+     * @param element The element to validate.
+     * @return A list of {@link Marker}s for the validated {@link GModelElement}.
+     */
+    doBatchValidation(element: GModelElement): Marker[] {
+        return [];
+    }
+
+    /**
+     * Perform a validation for a custom <code>reason</code> with the given <code>element</code>.
+     *
+     * <p>
+     * GLSP editors may add custom reasons for triggering a validation, other than <code>live</code> and
+     * <code>batch</code>.
+     * Validation requests that are not live or batch validations will be handled by this method.
+     * There is no need to traverse through the children in this method as {@link #validate(List, String)} will invoke
+     * this method for all children anyway.
+     * </p>
+     *
+     * @param element The element to validate.
+     * @return A list of {@link Marker}s for the validated {@link GModelElement}.
+     */
+    doValidationForCustomReason(element: GModelElement): Marker[] {
+        return [];
+    }
 }
